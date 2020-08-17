970e4cc7f974aa46464f16d29a1fda23b3f1e074
ariel murphy <arielladders@gmail.com>
2020-08-17 01:46:37 +0000

    added classes to cli

--- lib/Covid/cli.rb
+++ lib/Covid/cli.rb
@@ -1,11 +1,17 @@
 class CLI 
-  #needs to greet user, needs to run (execute) needs a menu, exit 
+  #needs to greet user, needs to run (execute) needs a menu, exit, need menu to choose states, and menu to choose counties of state (getter data)
   
   def greeting
     puts "Welcome to your Covid-19 County Planner."
   end 
   
   def menu
+    puts "Please choose a state to see confirmed cases of:"
+    list_states
+  end 
+  
+  
+  def list_states
   end 
   
   def end_program
