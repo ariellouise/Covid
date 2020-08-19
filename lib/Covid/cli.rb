cd1ab188fe8c4dbde282738ffc9344f1d31c0c4d
ariel murphy <arielladders@gmail.com>
2020-08-19 09:54:41 +0000

    list states

--- lib/Covid/cli.rb
+++ lib/Covid/cli.rb
@@ -15,11 +15,16 @@ class CLI
   
   def menu
     puts "Please choose a state:"
+    list_states
     input = gets.strip.downcase
     return input
   end 
- 
-
+  
+  def list_states
+    1. New York 
+    2. Washington
+    3. California
+  end 
  
  def end_program 
    puts "Remember that regardless of whichever county you visit, you should regularly wash your hands, maintain 6 feet distance from others, and wear a mask. Goodbye!"
