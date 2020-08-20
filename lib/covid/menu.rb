80b183d877bf9ff4da0deac0131b556e45f09892
ariel murphy <arielladders@gmail.com>
2020-08-20 16:35:03 +0000

    menu updated

--- lib/covid/menu.rb
+++ lib/covid/menu.rb
@@ -2,8 +2,29 @@ module Covid
   class Menu 
     
     def start 
-      put "Did this load?"
     end 
+    
+    def main Menu
+      puts "What State are you interested in the Covid Statistics of?"
+      
+      puts "1. New York"
+      puts "2. Washington"
+      puts "3. California"
+      
+      puts "Enter Choice Number"
+      choice = gets.strip 
+      
+      case "1" 
+        new_york 
+      case "2"
+        washington
+      case "3"
+        california
+      end 
+      
+    end 
+    
+    def 
     
   end 
 end 
