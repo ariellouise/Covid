2331287aece5da1907f2862d0b9afd4347d3b5d5
ariel murphy <arielladders@gmail.com>
2020-08-21 04:46:34 +0000

    modify menu

--- lib/covid/menu.rb
+++ lib/covid/menu.rb
@@ -6,7 +6,33 @@ module Covid
     end 
     
     def main Menu
-      puts "What 
+      puts "What statistics are you interested in?"
+      puts "1. Total Cases for the United States."
+      puts "2. Total Cases by state."
+      puts "3. Exit"
+      
+      puts "Enter number"
+      choice = gets.strip
+      
+      case "1"
+        us_stats
+      when "2"
+        states_menu
+      when "3"
+        end_program
+      end 
+    end 
+    
+    def us_stats 
+      puts "You wanted to know the total Covid-19 cases in the United States." 
+      main_menu
+    end 
+    
+    def states_menu
+      puts "You asked for the states menu to pick a state regarding their total Covid-19 cases."
+      main_menu
+    end 
+    
 
 "
       
