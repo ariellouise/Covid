ea023abdfb2da79f1a64c43e71e300638164025c
ariel murphy <arielladders@gmail.com>
2020-08-21 18:44:23 +0000

    modify scraper

--- lib/covid/scraper.rb
+++ lib/covid/scraper.rb
@@ -6,6 +6,9 @@ module Covid
     
     def load
       @doc = Nokogiri::HTML(open('https://www.worldometers.info/coronavirus/country/us/'))
+      @doc.search("usa_table_countries_today tbody tr").each do |table_row|
+        name_column = table_row.search("td") [0]
+        cases_column = table_row.search("td") [1]
     end 
     
     
