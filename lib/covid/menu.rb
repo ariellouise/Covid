e66cf6215089dfe4a45c66fb83e989aaa8e257b1
ariel murphy <arielladders@gmail.com>
2020-09-04 03:24:07 +0000

    modified menu

--- lib/covid/menu.rb
+++ lib/covid/menu.rb
@@ -1,9 +1,19 @@
 class Menu 
   
   def call 
+    puts "Welcome to the Covid-19 Statistic CLI."
+    scrape_states
+    list_states
   end 
   
   def list_states
+    puts "Would you like to choose to see Covid-19 statistics by state, or as a total list? Please choose a number:"
+    puts "  "
+    puts "Type '1' to choose from a list of states to see it's individual statistics."
+    puts "Type '2' to see a total list of Covid-19 cases for all states."
+    puts "Type '3' to see a total list of Covid-19 deaths for all states."
+    puts "Type '4' to see a total list of Covid-19 recoveries for all states."
+    puts "Type '5' to exit the program."
   end 
   
   def scrape_states
