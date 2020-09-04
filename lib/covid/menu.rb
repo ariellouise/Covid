fe2b059d8bd293dfad7eb3d93b34cbef2d4ccc73
ariel murphy <arielladders@gmail.com>
2020-09-04 02:20:11 +0000

    modified menu

--- lib/covid/menu.rb
+++ lib/covid/menu.rb
@@ -10,13 +10,45 @@ class Menu
     @end_program = end_program
   end 
   
-  def main_menu 
-    @main_menu 
+  def greeting 
+    puts "Welcome to the Covid-19 Statistic CLI."
+    scrape
   end 
+  
+  def main_menu 
+    puts "What statistics are you interested in?"
+    puts "1. Choose an individual state."
+    puts "2. Total Covid Cases by state."
+    puts "3. Total Covid Deaths by state."
+    puts "4. Total Covid Recoveries by state."
+    puts "5. Exit program."
+  end 
+  
+  def user_input(main_menu)
+    case option
+    when "1"
+      self.list_states
+    when "2"
+      puts "Total Covid Cases by state:"
+      State.all.each { |n| n.show_total_cases }
+    when "3"
+      puts "Total Covid Deaths by state:"
+      State.all.each { |n| n.show_total_deaths }
+    when "4"
+      puts "Total Covid Recoveries by state:"
+      State.all.each { |n| n.show_total_recoveries }
+    when "5"
+      end_program
+      exit
+    end 
+  end 
+
   
   def state_menu
     @state_menu
   end 
+  
+  def 
   
   def display
     @display
@@ -35,6 +67,7 @@ class Menu
     puts "Please remember to wash your hands and have a socially-distanced day!"
     puts " "
   end 
+  
   
   def scrape
     Scraper.scrape_states
