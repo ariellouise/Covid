7e16f6fa310dc949fe33f328d25aac7e00c1db18
ariel murphy <arielladders@gmail.com>
2020-09-04 02:11:00 +0000

    modified menu

--- lib/covid/menu.rb
+++ lib/covid/menu.rb
@@ -14,18 +14,48 @@ class Menu
     @state_menu
   end 
   
+  def display=(display)
+    @display = display
+  end 
+  
   def display
     @display
+  end 
+  
+  def options=(options)
+    @options = options
   end 
   
   def options 
     @options
   end 
   
+  def user_input=(user_input)
+    @user_input = user_input 
+  end 
+  
   def user_input 
     @user_input
   end 
   
+  
+  def end_program=(end_program)
+    @end_program = end_program 
+  end 
+  
   def end_program
-    @end_program
+     puts " "
+    puts "Please remember to wash your hands and have a socially-distanced day!"
+    puts " "
+  end 
+  
+  def scrape
+    Scraper.scrape_states
+  end 
+  
+  def list_states
+    State.all.each_with_index  do |state, i|
+      puts " #{i+1}. #{state.name}  "
+      puts ""
+     end
   end 
